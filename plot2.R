03fef90157ab473af01f2fa3f20c3c777c3a16ec
Onika Bevny <72917399+onikabevny@users.noreply.github.com>
2025-02-03 11:34:54 -0400

    plot2.R

--- plot2.R
+++ plot2.R
@@ -1,10 +1,23 @@
-dataFile <- "./data/household_power_consumption.txt"
-data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+# Defining file path
+dataFile <- "C:/Users/obevny/Downloads/exdata_data_household_power_consumption/household_power_consumption.txt"
 
-#str(subSetData)
-datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+# Reading the data, treating "?" as NA
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".", na.strings="?")
+
+# Convert Date column to Date format
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+
+# Subset data for the required dates
+subSetData <- subset(data, Date %in% as.Date(c("2007-02-01", "2007-02-02")))
+
+# Combining Date and Time into a POSIXlt datetime object
+datetime <- strptime(paste(subSetData$Date, subSetData$Time), format="%Y-%m-%d %H:%M:%S")
+
+# Converting Global_active_power to numeric, handling NAs
 globalActivePower <- as.numeric(subSetData$Global_active_power)
+
+# Creating and saving the line plot
 png("plot2.png", width=480, height=480)
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
+
