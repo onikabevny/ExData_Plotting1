1527b20e7c39ad97c68014097302fe391dbc28b8
Onika Bevny <72917399+onikabevny@users.noreply.github.com>
2025-02-03 11:21:56 -0400

    plot1.R

--- plot1.R
+++ plot1.R
@@ -1,9 +1,19 @@
-dataFile <- "./data/household_power_consumption.txt"
-data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+# Define file path
+dataFile <- "C:/Users/obevny/Downloads/exdata_data_household_power_consumption/household_power_consumption.txt"
 
-#str(subSetData)
+# Reading the data, treating "?" as NA
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".", na.strings="?")
+
+# Converting Date column to Date format
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+
+# Subset data for the required dates
+subSetData <- subset(data, Date %in% as.Date(c("2007-02-01", "2007-02-02")))
+
+# Converting Global_active_power to numeric
 globalActivePower <- as.numeric(subSetData$Global_active_power)
+
+# Crreating and saving histogram
 png("plot1.png", width=480, height=480)
-hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
-dev.off()
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)", breaks=50)
+
